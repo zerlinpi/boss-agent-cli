d5002a1d02184c22e98fa2c7754349d3e08284ba
zerlinpi <105773292+zerlinpi@users.noreply.github.com>
2026-08-07 15:08:31 +0800

    refactor(recruiter): keep Web extension bootstrap explicit

--- src/boss_agent_cli/web/__init__.py
+++ src/boss_agent_cli/web/__init__.py
@@ -1,12 +1,13 @@
 """Local graphical recruiter console."""
 
+from boss_agent_cli.web import controller as _controller
 from boss_agent_cli.web.contact_extension import install_contact_assets
 from boss_agent_cli.web.controller import RecruiterWebController, WebConsoleError
 from boss_agent_cli.web.export_security import install_export_security
 from boss_agent_cli.web.lifecycle import install_controller_extensions, install_server_extensions
 from boss_agent_cli.web.reply_extension import install_reply_assets
 
-install_export_security(__import__("boss_agent_cli.web.controller", fromlist=["*"]))
+install_export_security(_controller)
 install_controller_extensions()
 
 from boss_agent_cli.web import server as _server  # noqa: E402
