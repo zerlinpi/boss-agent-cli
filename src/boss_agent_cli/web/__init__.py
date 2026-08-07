011d4f86cb66e29373a036a0ced87f64760ca510
zerlinpi <105773292+zerlinpi@users.noreply.github.com>
2026-08-07 17:35:07 +0800

    fix: scope automatic BOSS drafts to current run

--- src/boss_agent_cli/web/__init__.py
+++ src/boss_agent_cli/web/__init__.py
@@ -2,6 +2,7 @@
 
 from boss_agent_cli.web import controller as _controller
 from boss_agent_cli.web import tasks as _tasks
+from boss_agent_cli.web.boss_draft_scope import install_boss_draft_scope
 from boss_agent_cli.web.contact_extension import install_contact_assets
 from boss_agent_cli.web.controller import RecruiterWebController, WebConsoleError
 from boss_agent_cli.web.export_security import install_export_security
@@ -14,6 +15,7 @@ from boss_agent_cli.web.ui_extension import install_ui_reliability_assets
 install_export_security(_controller)
 install_controller_extensions()
 install_controller_reliability()
+install_boss_draft_scope()
 install_task_manager_controls(_tasks)
 
 from boss_agent_cli.web import server as _server  # noqa: E402
